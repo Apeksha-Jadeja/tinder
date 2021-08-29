e17b4451f871dc32d9acd553e7693c1002175722
Apeksha Jadeja <81768737+Apeksha-Jadeja@users.noreply.github.com>
2021-08-30 02:47:11 +0530

    swipebuttons

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 
+import TinderCard from 'react-tinder-card';
 import './App.css';
+import Header from './Header';
+import TinderCards from './TinderCards';
 
 function App() {
   return (
     <div className="App">
-      <h1>HEYYY MERN STACK</h1>
+    {/* header */}
+
+    <Header/>
+    {/* TinderCards */}
+    <TinderCards/>
+    {/* buttons */}
+    <SwipeButtons/>
     </div>
   );
 }
