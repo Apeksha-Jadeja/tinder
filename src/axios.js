e5951d31d25213866d666a4323958cc0258cf359
Apeksha Jadeja <81768737+Apeksha-Jadeja@users.noreply.github.com>
2021-08-31 22:09:49 +0530

    h

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 const instance=axios.create({
-    baseURL:'https://aj-tinder-backend.herokuapp.com/'
+    baseURL:'https://aj-tinder-backend.herokuapp.com'
 })
 export default instance;
